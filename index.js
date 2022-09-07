ebc8914825d390d594328db893717ad5bd884c82
rajaomariajaona <rajaomariajaona11@gmail.com>
2022-09-07 07:03:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { convert } = require('convert-svg-to-png');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors())
+app.use(express.text())
+
+app.post('/save-png', async(req, res) => {
+  console.log(req.body);
+  const png = await convert(req.body);
+
+  res.set('Content-Type', 'image/png');
+  res.send(png);
+});
+app.post("/save-svg", async(req, res) => {
+    res.set('Content-Type', 'image/svg+xml');
+    res.send(req.body);
+})
+
+app.listen(3000);
